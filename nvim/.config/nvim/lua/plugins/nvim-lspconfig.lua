5f0ac5156db697cf6999fc24ebbcaccc50988e63
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-06-19 08:55:30 +0200

    feat: added omnisharp to lsp list (nvim)

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -181,6 +181,7 @@ return { -- LSP Configuration & Plugins
           hoverProvider = false,
         },
       },
+      omnisharp = {},
       rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
