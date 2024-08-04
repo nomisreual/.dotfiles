418aac44bb25df5a630919dbd65aeb0747112072
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-08-04 18:47:31 +0200

    fix: which-key syntax

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -148,7 +148,7 @@ return { -- LSP Configuration & Plugins
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
       -- clangd = {},
-      -- gopls = {},
+      gopls = {},
       -- pyright = {
       --   root_dir = util.root_pattern(unpack(root_files_python)),
       --   settings = {
